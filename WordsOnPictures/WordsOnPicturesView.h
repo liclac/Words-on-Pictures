eb092fbc9dbe6502ccc041c254fb5438a4f709c0
uppfinnarn <uppfinnarn@gmail.com>
2014-03-04 18:25:39 +0100

    Initial Commit

---
+++ WordsOnPictures/WordsOnPicturesView.h
@@ -0,0 +1,13 @@
+//
+//  WordsOnPicturesView.h
+//  WordsOnPictures
+//
+//  Created by Johannes Ekberg on 2014-03-04.
+//  Copyright (c) 2014 MacaroniCode. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface WordsOnPicturesView : ScreenSaverView
+
+@end
---
+++ WordsOnPictures/WordsOnPicturesView.m
@@ -0,0 +1,52 @@
+//
+//  WordsOnPicturesView.m
+//  WordsOnPictures
+//
+//  Created by Johannes Ekberg on 2014-03-04.
+//  Copyright (c) 2014 MacaroniCode. All rights reserved.
+//
+
+#import "WordsOnPicturesView.h"
+
+@implementation WordsOnPicturesView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
